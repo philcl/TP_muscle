85048520ed6ac3dff9539c7bb817a56cf72a2f1a
philcl <phil13420@live.fr>
2018-12-24 11:36:35 +0100

    Ajout prix Exception + marketing

--- src/ObservateurClient.java
+++ src/ObservateurClient.java
@@ -6,8 +6,14 @@ public class ObservateurClient extends Superviseur {
     @Override
     public void notify(Object o) {
         Client c = (Client)o;
-        if(c.getPanier().calculDuPrix(c) > 100)
-            System.out.println();
+        try {
+            if(c.getPanier().calculDuPrix(c) > 100)
+                System.out.println("Le panier dépasse 100 €");
+        }
+        catch(PrixException e) {
+            System.err.println("Le prix est négatif");
+        }
+
 
 
     }
